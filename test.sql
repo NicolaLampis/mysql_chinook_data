b67c909f7fb300f83c0bdf80df39d0012703e966
NicolaLampis <nico.lampo@gmail.com>
2021-03-03 14:04:55 +0000

    working with data

--- test.sql
+++ test.sql
@@ -39,3 +39,45 @@ select EmployeeId, LastName, FirstName, HireDate from Employee
 order by HireDate desc, EmployeeId desc
 limit 3;
 
+
+select Track.Name as Track, sum(InvoiceLine.UnitPrice * InvoiceLine.Quantity) from Track 
+inner join InvoiceLine on Track.TrackId = InvoiceLine.TrackId
+where Track.Name = "The Woman King";
+
+
+SELECT Artist.Name AS Artist, COUNT(Track.TrackId) AS Track FROM Artist
+JOIN Album ON Artist.ArtistId = Album.ArtistId
+JOIN Track ON Album.AlbumId = Track.AlbumId
+GROUP BY Artist.Name
+ORDER BY COUNT(Artist.Name)
+DESC LIMIT 5;
+
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("Twilight", 348, 2, 1, "U2", 210000, 1234, 0.99);
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("An Cat Dubh", 348, 2, 1, "U2", 110000, 1234, 0.99);
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("Into the Heart", 348, 2, 1, "U2", 200000, 1234, 0.99);
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("Out of Control", 348, 2, 1, "U2", 100000, 1234, 0.99);
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("Stories for Boys", 348, 2, 1, "U2", 150000, 1234, 0.99);
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("The Ocean", 348, 2, 1, "U2", 175000, 1234, 0.99);
+
+INSERT INTO Track (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES ("A Day Without Me", 348, 2, 1, "U2", 185000, 1234, 0.99);
+
+
+INSERT INTO Track
+    (Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice)
+VALUES
+    ("Another Time, Another Place", 348, 2, 1, "U2", 210000, 1234, 0.99),
+    ("The Electric Co.", 348, 2, 1, "U2", 200000, 1234, 0.99),
+    ("Shadows and Tall Trees", 348, 2, 1, "U2", 150000, 1234, 0.99);
